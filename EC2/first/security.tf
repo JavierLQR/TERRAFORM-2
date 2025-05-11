40c1ecf3d6640a10ce20117359e35e90acc5a5b7
rodrigo15975 <javierluis15975@hotmail.com>
2025-05-11 02:42:57 -0500

    feat: create security.tf

---
+++ EC2/first/security.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "nginx_security_group" {
+  description = "Security group for nginx server"
+  name        = var.server_name
+
+  ingress {
+    description = "Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    description = "Port 0"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name     = var.server_name
+    NODE_ENV = var.environment
+  }
+}
