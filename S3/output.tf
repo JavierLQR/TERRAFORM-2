090be84141c9bf5a0fa1a006d6ac28cca4315595
rodrigo15975 <javierluis15975@hotmail.com>
2025-05-12 00:27:29 -0500

    feat: create output.tf

---
+++ S3/output.tf
@@ -0,0 +1,18 @@
+
+output "access_key_id" {
+  value       = aws_iam_access_key.dev_access_key.id
+  sensitive   = true
+  description = "Access Key para el usuario dev"
+}
+
+output "secret_access_key" {
+  value       = aws_iam_access_key.dev_access_key.secret
+  sensitive   = true
+  description = "Secret Key para el usuario dev"
+}
+
+output "name_bucket" {
+  value       = aws_s3_bucket.mi_bucket_s3.id
+  description = "Name of the bucket"
+  sensitive   = false
+}
