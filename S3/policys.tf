61919baf1f7b8625db689fc7ccf7d5e86870d10f
rodrigo15975 <javierluis15975@hotmail.com>
2025-05-12 00:55:24 -0500

    update policys.tf

--- S3/policys.tf
+++ S3/policys.tf
@@ -2,6 +2,10 @@
 # Configuración de ACL para el bucket( bucket )
 resource "aws_s3_bucket_policy" "dev_bucket_policy" {
   bucket = aws_s3_bucket.mi_bucket_s3.id
+
+  # La política debe estar asociada al bucket para que se aplique
+  depends_on = [aws_s3_bucket_public_access_block.mi_bucket_public_access_block]
+
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
