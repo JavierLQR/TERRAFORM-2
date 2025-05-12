ddf0ceeb03cd9e60894511cff6d2af2f1633be4d
rodrigo15975 <javierluis15975@hotmail.com>
2025-05-12 00:22:08 -0500

    update main.tf

--- S3/main.tf
+++ S3/main.tf
@@ -105,10 +105,30 @@ resource "aws_iam_policy" "dev_s3_policy" {
     Version = "2012-10-17"
     Statement = [
       {
+        Sid    = "DevS3Policy"
         Effect = "Allow"
         Action = [
           "s3:GetObject",
           "s3:PutObject"
+        ]
+        Resource = "${aws_s3_bucket.mi_bucket_s3.arn}/*"
+      }
+    ]
+  })
+}
+
+# Configuración de ACL para el bucket
+resource "aws_s3_bucket_policy" "dev_bucket_policy" {
+  bucket = aws_s3_bucket.mi_bucket_s3.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadAccess",
+        Effect    = "Allow"
+        Principal = "*"
+        Action = [
+          "s3:GetObject",
         ]
         Resource = "${aws_s3_bucket.mi_bucket_s3.arn}/*"
       }
