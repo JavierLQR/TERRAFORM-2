a6fe925bab7355b45e7d701d95efa116477a0000
rodrigo15975 <javierluis15975@hotmail.com>
2025-05-13 01:28:13 -0500

    update main.tf

--- S3/main.tf
+++ S3/main.tf
@@ -48,21 +48,8 @@ resource "null_resource" "empty_bucket" {
 }
 
 
-# Configuración de ACL para el bucket
-# Recurso que configura el control de propiedad de objetos en un bucket S3
-resource "aws_s3_bucket_ownership_controls" "mi_bucket_ownership_controls" {
-  # Especificamos el ID del bucket al que se aplicará este control
-  bucket = aws_s3_bucket.mi_bucket_s3.id
-  # Definimos la regla de propiedad de los objetos
-  rule {
-    # Esta opción "BucketOwnerEnforced" hace que:
-    # - El propietario del bucket será el propietario de todos los objetos cargados,
-    #   incluso si fueron subidos por otro usuario o cuenta.
-    # - Las ACLs (listas de control de acceso) quedan completamente desactivadas.
-    #   Es decir, no se puede usar "public-read" ni otras ACLs.
-    object_ownership = "BucketOwnerEnforced"
-  }
-}
+
+
 
 # Configuración de ACL para el bucket
 resource "aws_s3_bucket_public_access_block" "mi_bucket_public_access_block" {
