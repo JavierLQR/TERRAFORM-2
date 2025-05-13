c4a40d43c77666b8e911ee95fd5961fddc349632
rodrigo15975 <javierluis15975@hotmail.com>
2025-05-13 01:36:56 -0500

    update ownership-controls.tf

--- S3/ownership-controls.tf
+++ S3/ownership-controls.tf
@@ -14,3 +14,15 @@ resource "aws_s3_bucket_ownership_controls" "mi_bucket_ownership_controls" {
     object_ownership = "BucketOwnerEnforced"
   }
 }
+
+
+
+# Configuración de ACL para el bucket
+resource "aws_s3_bucket_public_access_block" "mi_bucket_public_access_block" {
+  bucket = aws_s3_bucket.mi_bucket_s3.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
