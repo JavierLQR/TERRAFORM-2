0edaae4f1dc9402285ae55b2fc63f0884978fc2f
rodrigo15975 <javierluis15975@hotmail.com>
2025-05-18 03:39:05 -0500

    update main.tf

--- RDS/first/main.tf
+++ RDS/first/main.tf
@@ -2,6 +2,17 @@ variable "region" {
   default = "us-east-1"
 }
 
+variable "db_username" {
+  default = "admin"
+}
+
+variable "db_password" {
+  default = "mysecretpassword"
+}
+
+variable "db_name" {
+  default = "mydatabase"
+}
 provider "aws" {
   region = "us-east-1"
 }
