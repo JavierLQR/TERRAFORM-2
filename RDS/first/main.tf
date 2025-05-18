ed7e7df63ad2410192cc50fb9056d8f5e303b9ff
rodrigo15975 <javierluis15975@hotmail.com>
2025-05-18 03:36:04 -0500

    feat: add security group for RDS with ingress and egress rules

--- RDS/first/main.tf
+++ RDS/first/main.tf
@@ -12,3 +12,24 @@ resource "aws_subnet" "private" {
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 }
+
+
+# Security Group para RDS
+resource "aws_security_group" "rds_sg" {
+  name   = "rds_sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
