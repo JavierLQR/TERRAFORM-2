9e169b449464979a1db69205f020605305400641
rodrigo15975 <javierluis15975@hotmail.com>
2025-05-18 03:35:47 -0500

    update main.tf

--- RDS/first/main.tf
+++ RDS/first/main.tf
@@ -6,3 +6,9 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
