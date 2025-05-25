b240f505dcace33bc7be0ca06fccc56b6eec064b
rodrigo15975 <javierluis15975@hotmail.com>
2025-05-24 23:19:53 -0500

    update variables.tf

--- SECRETS-MANAGERS/variables.tf
+++ SECRETS-MANAGERS/variables.tf
@@ -3,7 +3,6 @@ variable "env" {
   default     = "dev"
   type        = string
   nullable    = false
-  sensitive   = false
   validation {
     condition     = contains(["dev", "prod"], var.env)
     error_message = "Environment must be either 'dev' or 'prod'."
