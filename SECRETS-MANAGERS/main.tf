0056f73c9347b084b4d70b6b7f6552a164b7e49c
rodrigo15975 <javierluis15975@hotmail.com>
2025-05-24 22:51:03 -0500

    update main.tf

--- SECRETS-MANAGERS/main.tf
+++ SECRETS-MANAGERS/main.tf
@@ -1,15 +1,3 @@
-variable "env" {
-  description = "Environment name (e.g., dev, prod)"
-  default     = "dev"
-  type        = string
-  nullable    = false
-  validation {
-    condition     = contains(["dev", "prod"], var.env)
-    error_message = "Environment must be either 'dev' or 'prod'."
-  }
-
-}
-
 
 
 variable "region" {
