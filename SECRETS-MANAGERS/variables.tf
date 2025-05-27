a829210f5a91c9657c35229fe8edb1f96fb06c05
rodrigo15975 <javierluis15975@hotmail.com>
2025-05-27 01:36:22 -0500

    update variables.tf

--- SECRETS-MANAGERS/variables.tf
+++ SECRETS-MANAGERS/variables.tf
@@ -1,10 +1,10 @@
-variable "env" {
+variable "env_test" {
   description = "Environment name (e.g., dev, prod)"
   default     = "dev-test"
   type        = string
   nullable    = false
   validation {
-    condition     = contains(["dev", "prod"], var.env)
+    condition     = contains(["dev-test", "prod-test"], var.env_test)
     error_message = "Environment must be either 'dev' or 'prod'."
   }
 }
