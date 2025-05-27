f0e3e65128dfa9706290ed96a267edafbf985367
rodrigo15975 <javierluis15975@hotmail.com>
2025-05-27 01:31:25 -0500

    update output.tf

--- SECRETS-MANAGERS/output.tf
+++ SECRETS-MANAGERS/output.tf
@@ -1,4 +1,4 @@
-output "show_password_arn_db_password" {
+output "secret_password_arn" {
   description = "ARN of the Secrets Manager secret for the database password"
   value       = aws_secretsmanager_secret.db_password.arn
 
