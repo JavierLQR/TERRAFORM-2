161c101c65e1cf2311dff8a91cdc554ec28d2190
rodrigo15975 <javierluis15975@hotmail.com>
2025-05-27 23:46:30 -0500

    update main.tf

--- SECRETS-MANAGERS/two/main.tf
+++ SECRETS-MANAGERS/two/main.tf
@@ -33,7 +33,6 @@ output "rabbitmq_url_arn" {
 }
 
 
-# Outputs for the RabbitMQ URL secret version
 output "rabbitmq_url" {
   description = "RabbitMQ URL secret value"
   value       = aws_secretsmanager_secret_version.RABBIT_URL.secret_string
