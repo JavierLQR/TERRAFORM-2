540d0f36f87f22ad289b0e31a413e8de3f2648d1
rodrigo15975 <javierluis15975@hotmail.com>
2025-05-29 23:41:54 -0500

    feat: create variables.tf

---
+++ EKS/variables.tf
@@ -0,0 +1,11 @@
+variable "environment" {
+  description = "Environment"
+  type        = string
+  default     = "dev"
+}
+
+variable "type_instance" {
+  description = "Instance type"
+  type        = string
+  default     = "t2.micro"
+}
