e413fc35ac34ea38e32a8353e0b5b3242c3e7ee7
rodrigo15975 <javierluis15975@hotmail.com>
2025-05-30 23:05:50 -0500

    update variables.tf

--- EKS/variables.tf
+++ EKS/variables.tf
@@ -4,4 +4,13 @@ variable "environment" {
   default     = "dev"
 }
 
+variable "region_name" {
+  description = "Region"
+  type        = string
+  default     = "us-east-1"
+  validation {
+    condition     = contains(["us-east-1", "us-west-2"], var.region_name)
+    error_message = "Region must be either 'us-east-1' or 'us-west-2'."
+  }
 
+}
