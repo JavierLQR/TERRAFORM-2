a4fa2a8ffabc7331dd5706f6d0a53f2ca4e4ea11
rodrigo15975 <javierluis15975@hotmail.com>
2025-05-31 01:11:46 -0500

    update main.tf

--- EKS/main.tf
+++ EKS/main.tf
@@ -1,12 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region_name
 }
 
+resource "random_pet" "this" {}
 
-# Random suffix para recursos únicos
-resource "random_pet" "eks" {}
-
-# 1. VPC y subnets (simple con módulo)
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.1"
@@ -14,7 +24,7 @@ module "vpc" {
   name = "eks-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${var.region_name}", "${var.region_name}"]
+  azs             = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
 
@@ -22,23 +32,20 @@ module "vpc" {
   single_nat_gateway = true
 }
 
-# 2. EKS Cluster
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.5"
 
-  cluster_name    = "${var.cluster_name}-${random_pet.eks.id}"
+  cluster_name    = "eks-${random_pet.this.id}"
   cluster_version = "1.29"
   subnet_ids      = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
-  # 3. Nodo de prueba
   eks_managed_node_groups = {
     default = {
-      desired_size = 2
-      max_size     = 3
-      min_size     = 1
-
+      desired_size   = 2
+      min_size       = 1
+      max_size       = 3
       instance_types = ["t3.small"]
     }
   }
