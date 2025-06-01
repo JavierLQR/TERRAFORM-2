6f7cb087a9ad6f5b2e1f81346faa7da111fa2fd3
rodrigo15975 <javierluis15975@hotmail.com>
2025-05-31 20:41:21 -0500

    update main.tf

--- EKS/main.tf
+++ EKS/main.tf
@@ -36,11 +36,13 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.5"
 
-  cluster_name    = "eks-${random_pet.this.id}"
-  cluster_version = "1.29"
+  cluster_name    = "nestjs-terraform-eks-cluster"
+  cluster_version = "1.31"
   subnet_ids      = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
+  # OIDC (enabled by default en v20+)
+  enable_irsa = true
   eks_managed_node_groups = {
     default = {
       desired_size   = 2
