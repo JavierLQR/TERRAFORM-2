13fb4c272d6d8b6825f9eeb8322e0873c31550ee
rodrigo15975 <javierluis15975@hotmail.com>
2025-06-03 23:26:42 -0500

    update variables.tf

--- EKS/two/variables.tf
+++ EKS/two/variables.tf
@@ -2,3 +2,10 @@ variable "aws_region" {
   default     = "us-east-1"
   description = "AWS region for the EKS cluster"
 }
+
+variable "type_instance" {
+  type        = string
+  default     = "t3.medium"
+  description = "Instance type"
+
+}
