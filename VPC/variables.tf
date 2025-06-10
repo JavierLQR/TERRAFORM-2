fd478d656b5f5b31f6b5e7abc9d3343d277fb610
rodrigo15975 <javierluis15975@hotmail.com>
2025-06-09 21:35:04 -0500

    update variables.tf

--- VPC/variables.tf
+++ VPC/variables.tf
@@ -26,10 +26,3 @@ variable "team" {
   type        = string
 }
 
-variable "owner" {
-  default     = "rodrigo-${var.team}"
-  description = "Owner of the cluster"
-  sensitive   = false
-  nullable    = false
-
-}
