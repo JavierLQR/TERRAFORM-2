77e7579c4acfc3b57125b7b7efb6d9f85e941118
rodrigo15975 <javierluis15975@hotmail.com>
2025-06-11 22:00:50 -0500

    update variables.tf

--- VPC/variables.tf
+++ VPC/variables.tf
@@ -25,13 +25,3 @@ variable "team" {
   nullable    = false
   type        = string
 }
-
-
-variable "vpc_name" {
-  default     = "my-vpc"
-  description = "Name of the VPC"
-  sensitive   = false
-  nullable    = false
-  type        = string
-
-}
