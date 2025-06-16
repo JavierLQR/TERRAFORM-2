4f2e098915da2c990ccbd7739917304debb5f1c0
rodrigo15975 <javierluis15975@hotmail.com>
2025-06-15 22:59:54 -0500

    update main.tf

--- VPC/main.tf
+++ VPC/main.tf
@@ -1,3 +1,48 @@
 provider "aws" {
   region = var.region
 }
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr_block
+
+  tags = {
+    Name = "MiVPC",
+    OwnendBy = var.team
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = var.availability_zone
+
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "InternetGateway"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "PublicRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
